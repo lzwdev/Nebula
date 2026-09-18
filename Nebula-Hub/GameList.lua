e987f649be41c1c35bb73f5b443eafbea590c1fd
lzwdev <lzhenwei.dev@gmail.com>
2026-09-18 13:15:51 +0200

    Update GameList.lua

--- Nebula-Hub/GameList.lua
+++ Nebula-Hub/GameList.lua
@@ -1,32 +1,32 @@
 return {
     [142823291] = {
         Name = "Murder Mystery 2",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/MM2.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/MM2.lua",
         Description = "ESP Roles, Aimbot, Noclip"
     },
     ["UNIVERSAL"] = {
         Name = "Universal Admin",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/admin.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/admin.lua",
         Description = "Fly, Noclip, ESP, Godmode, Touchfling"
     },
     [17664219670] = {
         Name = "Airstrikes NPCs",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/GamepassFree.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Gamepasses for free, infinite jump"
     },
     [189707] = {
         Name = "Natural Disaster Survival",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/Natural-Disaster-Survival.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Fly, Noclip, ESP, Godmode"
     },
     [174238106] = {
         Name = "RED VS BLUE PLANE WARS",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/RedVSBlue-PlaneWars.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Auto farm, Killall, ESP, Serverhop"
     },
     [4325400580] = {
         Name = "Army Tycoon",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/Army-Tycoon.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Kill All Units, Get All Buildings, Lag Players, Nuke to Mouse (Q), Give All Players Buildings"
     },
     [128784467030899] = {
@@ -36,17 +36,17 @@ return {
     },
     [654732683] = {
         Name = "Car Crushers 2",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/CarCrushers2.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Car Speed Hack, Auto Farm Money, Troll Players"
     },
     [994732206] = {
         Name = "Blox Fruits",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/ChestFarm-FruitFarm.luau",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Chest Farm, Fruit Farm"
     },
     [95082159892680] = {
         Name = "1 Speed Keyboard Escape Candy Chocolate",
-        Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/+1-KeyboardEscape.lua",
+        Script = "https://github.com/lzwdev/Nebula/raw/refs/heads/main/Nebula-Hub/Scripts/",
         Description = "Auto Farm, Godmode, Get Infinite Trail for free"
     }
 }
